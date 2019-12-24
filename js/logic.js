0b42a9df13d625817c9dc3b5236e0200673a6992
Mykola Bartkiv <bartkivmm93@gmail.com>
2019-12-24 16:05:30 +0200

    Update logic.js

--- js/logic.js
+++ js/logic.js
@@ -38,6 +38,36 @@ $(document).ready(function () {
         var t = $(this);
         if(t.html().replace(/\s|&nbsp;/g, '').length === 0) { t.addClass('jEmpty'); }
     });*/
+    
+    $('img.img_svg').each(function(){
+        var $img = jQuery(this);
+        var imgID = $img.attr('id');
+        var imgClass = $img.attr('class');
+        var imgURL = $img.attr('src');
+
+        $.get(imgURL, function(data) {
+            var $svg = jQuery(data).find('svg');
+
+            if(typeof imgID !== 'undefined') {
+                $svg = $svg.attr('id', imgID);
+            }
+
+
+            if(typeof imgClass !== 'undefined') {
+                $svg = $svg.attr('class', imgClass+' replaced-svg');
+            }
+
+            $svg = $svg.removeAttr('xmlns:a');
+
+            if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+                $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+            }
+
+            $img.replaceWith($svg);
+
+        }, 'xml');
+
+    });
 
 });
 

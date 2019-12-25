3ea72de87c211d6bd064ae24b31afa44b615ff18
Mykola Bartkiv <bartkivmm93@gmail.com>
2019-12-25 11:59:50 +0200

    Update tpl-custom.php

--- tpl-custom.php
+++ tpl-custom.php
@@ -1,9 +1,13 @@
 <?php get_header(); /* Template Name: Example */ ?>
     <section class="content">
         <div class="container">
-	        <?php if (have_posts()) : while (have_posts()) : the_post();
-		        the_content();
-	        endwhile; endif; ?>
+	  <?php if ( '' !== get_post()->post_content ) : ?>
+            <div class="text">
+                <?php while ( have_posts() ) : the_post(); ?>
+                    <?php the_content();; ?>
+                <?php endwhile; ?>
+            </div>
+          <?php endif; ?>
         </div>
     </section>
 <?php get_footer(); ?>

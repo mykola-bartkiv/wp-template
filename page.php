843246de720795575c145d3d587f6fbc5e181e76
Mykola Bartkiv <bartkivmm93@gmail.com>
2019-12-25 11:59:02 +0200

    Update page.php

--- page.php
+++ page.php
@@ -2,9 +2,13 @@
 <section class="content">
     <div class="container">
         <h1><?php the_title(); ?></h1>
-        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-            <?php the_content(); ?>
-        <?php endwhile; endif; ?>
+        <?php if ( '' !== get_post()->post_content ) : ?>
+            <div class="text">
+                <?php while ( have_posts() ) : the_post(); ?>
+                    <?php the_content();; ?>
+                <?php endwhile; ?>
+            </div>
+        <?php endif; ?>
     </div>
 </section>
 <?php get_footer(); ?>

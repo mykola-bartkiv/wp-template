19126f869c608cb68e1f4d3c93f99496a4d10403
Mykola Bartkiv <bartkivmm93@gmail.com>
2020-02-17 11:38:29 +0200

    Update index.php
    Update Index

--- index.php
+++ index.php
@@ -4,26 +4,17 @@
         <article>
             <h1><?php echo get_the_title(BLOG_ID); ?></h1>
             <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-                <div class="post">
-                    <?php if (has_post_thumbnail()) { ?>
-                        <a class="thumb" href="<?php the_permalink(); ?>">
-                            <?php the_post_thumbnail(); ?>
-                        </a>
-                    <?php } else {?>
-                        <a class="thumb" href="<?php the_permalink(); ?>">
-                            <img src="<?php echo theme() ?>/img/holder.png" alt="holder">
-                        </a>
-                    <?php } ?>
+                <a href="<?php the_permalink(); ?>" class="post">
+                    <figure class="thumb">
+                            <?php echo has_post_thumbnail() ? the_post_thumbnail() : '<img src="' . theme('/img/holder.png') . '" alt="holder">' ?>
+                     </figure>
                     <div class="info">
                         <time datetime="<?php echo get_the_date('Y-m-d'); ?>"><?php echo get_the_date('F j, Y'); ?></time>
-                        <div class="author_name">Author:
-                            <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ), get_the_author_meta( 'user_nicename' ) ); ?>"><?php the_author(); ?></a>
-                        </div>
-                        <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                        <h3><?php the_title(); ?></h3>
                         <p><?php echo wp_trim_words( strip_shortcodes( get_the_content() ), 20, "..." );  ?></p>
-                        <a class="more" href="<?php the_permalink(); ?>">Read more ></a>
+                        <span class="more">Read more ></span>
                     </div>
-                </div>
+                </a>
             <?php endwhile; endif; ?>
         </article>
         <?php echo ( is_active_sidebar( 'blog_sidebar' ) ) ? '<aside>'. dynamic_sidebar('blog_sidebar') .'</aside>' : ''; ?>

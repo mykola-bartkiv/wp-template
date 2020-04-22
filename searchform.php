b401fac55ee9fa1963778a72a83ad0e9f3e48f54
Mykola Bartkiv <bartkivmm93@gmail.com>
2020-04-22 15:30:50 +0300

    Update searchform.php

--- searchform.php
+++ searchform.php
@@ -1,4 +1,8 @@
-<form action="<?php echo site_url('/'); ?>" class="searchform" id="searchform" method="get">
-    <input type="text" placeholder="Search ..." id="s" name="s" value="">
-    <button type="submit" value="Search" id="searchsubmit" class="icon-search"></button>
-</form>
+<div class="search-box">
+    <form action="<?php echo site_url('/'); ?>" class="search-form" method="get">
+        <button type="submit" class="fa fa-search"></button>
+        <label for="s" style="display: none;">Search</label>
+        <input type="text" placeholder="Search" id="s" name="s" value="">
+    </form>
+    <div class="search-btn fa fa-search"></div>
+</div>

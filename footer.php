cb4c742daea6f5506769f244bb97e70b6b67014e
Mykola Bartkiv <bartkivmm93@gmail.com>
2021-01-29 16:10:16 +0200

    Add copyright template

--- footer.php
+++ footer.php
@@ -1,7 +1,9 @@
 </div>
 <footer>
     <div class="container">
-
+        <?php if ( $copyright = get_field( 'copyright', 'option' ) ) : ?>
+            <span class="copyright">Copyright &#169; <?php echo date( "Y" ); ?> <?php echo $copyright; ?></span>
+        <?php endif; ?>
     </div>
 </footer>
 <?php wp_footer(); ?>

44195f11e8e9afc9fa7d06b22bff04f9544a4f7c
Mykola Bartkiv <bartkivmm93@gmail.com>
2021-02-03 12:24:48 +0200

    Fix image breeding

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
             <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
                 <a href="<?php the_permalink(); ?>" class="post">
                     <figure class="thumb">
-                            <?php echo has_post_thumbnail() ? the_post_thumbnail() : '<img src="' . theme('/img/holder.png') . '" alt="holder">' ?>
+                            <?php echo has_post_thumbnail() ? get_the_post_thumbnail( get_the_ID(), 'medium_large' ) : '<img src="' . theme('/img/holder.png') . '" alt="holder">' ?>
                      </figure>
                     <div class="info">
                         <time datetime="<?php echo get_the_date('Y-m-d'); ?>"><?php echo get_the_date('F j, Y'); ?></time>

5f8f6743160461d7ed675f729d5637b48a4a75fa
Mykola Bartkiv <bartkivmm93@gmail.com>
2021-05-20 16:19:57 +0300

    Update Index

--- index.php
+++ index.php
@@ -1,23 +1,18 @@
 <?php get_header(); ?>
 <section class="content">
     <div class="container">
-        <article>
-            <h1><?php echo get_the_title(BLOG_ID); ?></h1>
-            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-                <a href="<?php the_permalink(); ?>" class="post">
-                    <figure class="thumb">
-                            <?php echo has_post_thumbnail() ? get_the_post_thumbnail( get_the_ID(), 'medium_large' ) : '<img src="' . theme('/img/holder.png') . '" alt="holder">' ?>
-                     </figure>
-                    <div class="info">
-                        <time datetime="<?php echo get_the_date('Y-m-d'); ?>"><?php echo get_the_date('F j, Y'); ?></time>
-                        <h3><?php the_title(); ?></h3>
-                        <p><?php echo wp_trim_words( strip_shortcodes( get_the_content() ), 20, "..." );  ?></p>
-                        <span class="more">Read more ></span>
-                    </div>
-                </a>
-            <?php endwhile; endif; ?>
-        </article>
-        <?php echo ( is_active_sidebar( 'blog_sidebar' ) ) ? '<aside>'. dynamic_sidebar('blog_sidebar') .'</aside>' : ''; ?>
+        <?php get_template_part( 'parts/social-icons' ); ?>
+        <h1>Test</h1>
+        <?php if ( '' !== get_post()->post_content ) : ?>
+            <div class="text">
+                <?php while ( have_posts() ) : the_post(); ?>
+                    <?php the_content();; ?>
+                <?php endwhile; ?>
+            </div>
+        <?php endif; ?>
     </div>
+    <?php if ( $image = get_field( 'image' ) ) : ?>
+        <div class="test" style="<?php echo image_src( $image, 'large', true, true ) ?>"></div>
+    <?php endif; ?>
 </section>
 <?php get_footer(); ?>

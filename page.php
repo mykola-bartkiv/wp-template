efd8a441796a4faaecb2ae3c2444047035c810a5
Mykola Bartkiv <bartkivmm93@gmail.com>
2021-07-21 15:46:56 +0300

    Update page.php

--- page.php
+++ page.php
@@ -5,7 +5,7 @@
         <?php if ( '' !== get_post()->post_content ) : ?>
             <div class="text last-no-spacing cfx">
                 <?php while ( have_posts() ) : the_post(); ?>
-                    <?php the_content();; ?>
+                    <?php the_content(); ?>
                 <?php endwhile; ?>
             </div>
         <?php endif; ?>

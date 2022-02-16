6549e3f07fd6e8158a24063c8248db51b4cb755d
Mykola Bartkiv <bartkivmm93@gmail.com>
2022-02-16 10:42:31 +0200

    Remove FA classes

--- searchform.php
+++ searchform.php
@@ -1,8 +1,8 @@
 <div class="search-box">
     <form action="<?php echo site_url('/'); ?>" class="search-form" method="get">
-        <button type="submit" class="fa fa-search"></button>
+        <button type="submit" class="inner-search-btn"></button>
         <label for="s" style="display: none;">Search</label>
-        <input type="text" placeholder="Search" id="s" name="s" value="">
+        <input type="text" placeholder="Search" name="s" value="">
     </form>
-    <div class="search-btn fa fa-search"></div>
+    <div class="search-btn"></div>
 </div>

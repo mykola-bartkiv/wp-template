a1f7199a4bec8b5ea076a692bd355707e785fd60
Mykola Bartkiv <bartkivmm93@gmail.com>
2023-02-21 12:20:22 +0200

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -4,7 +4,7 @@
          <?php if ( '' !== get_post()->post_content ) : ?>
             <div class="text">
                 <?php while ( have_posts() ) : the_post(); ?>
-                    <?php the_content();; ?>
+                    <?php the_content(); ?>
                 <?php endwhile; ?>
             </div>
         <?php endif; ?>

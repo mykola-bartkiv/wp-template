d1196fb40de0851f4156154bb9f6aa3c9c4e7839
Mykola Bartkiv <bartkivmm93@gmail.com>
2023-05-16 09:28:31 +0300

    Remove IMG to SVG

--- js/logic.js
+++ js/logic.js
@@ -56,38 +56,6 @@ $(document).ready(function () {
         });
     }
     //====End Search====
-
-    //====IMG to SVG====
-    $('img[src$=".svg"]').each(function () {
-        var $img = $(this);
-        var imgID = $img.attr('id');
-        var imgClass = $img.attr('class');
-        var imgURL = $img.attr('src');
-
-        $.get(imgURL, function (data) {
-            var $svg = $(data).find('svg');
-
-            if (typeof imgID !== 'undefined') {
-                $svg = $svg.attr('id', imgID);
-            }
-
-
-            if (typeof imgClass !== 'undefined') {
-                $svg = $svg.attr('class', imgClass + ' replaced-svg');
-            }
-
-            $svg = $svg.removeAttr('xmlns:a');
-
-            if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
-                $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
-            }
-
-            $img.replaceWith($svg);
-
-        }, null, 'xml');
-
-    });
-    //====End IMG to SVG====
     
     //==== Easy Scroll (button)====
     var btn = $('a.btn');

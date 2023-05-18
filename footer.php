58e6391f0cebe771f2a289215b7968a114e30829
Mykola Bartkiv <bartkivmm93@gmail.com>
2023-05-18 10:11:43 +0300

    Add Footer menu without child items

--- footer.php
+++ footer.php
@@ -1,6 +1,16 @@
 </div>
 <footer>
     <div class="container">
+        <?php if ( has_nav_menu( 'footer_menu' ) ) : ?>
+            <nav class="footer-menu">
+                <?php wp_nav_menu( array(
+                    'container'      => false,
+                    'items_wrap'     => '<ul id="%1$s">%3$s</ul>',
+                    'theme_location' => 'footer_menu',
+                    'depth'          => 1,
+                ) ); ?>
+            </nav>
+        <?php endif; ?>
         <?php if ( $copyright = get_field( 'copyright', 'option' ) ) : ?>
             <span class="copyright">Copyright &#169; <?php echo date( "Y" ); ?> <?php echo $copyright; ?></span>
         <?php endif; ?>

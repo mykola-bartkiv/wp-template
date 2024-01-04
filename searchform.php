61f1faceff8fe7dc72730b9b5c5ebe81743fd517
Mykola Bartkiv <bartkivmm93@gmail.com>
2024-01-04 17:22:34 +0200

    Add label to button

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <div class="search-box">
     <form action="<?php echo site_url('/'); ?>" class="search-form" method="get">
-        <button type="submit" class="inner-search-btn">
+        <button type="submit" class="inner-search-btn" aria-label="Search">
             <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" clip-rule="evenodd" d="M13.2891 15.0011C11.9034 16.0292 10.1869 16.6374 8.32814 16.6374C3.72864 16.6374 0 12.9131 0 8.3189C0 3.7247 3.72864 0.000366211 8.32814 0.000366211C12.9276 0.000366211 16.6563 3.7247 16.6563 8.3189C16.6563 10.1739 16.0484 11.8871 15.0207 13.2706L20.0001 18.2748L18.2628 19.9995L13.2891 15.0011ZM14.2066 8.31867C14.2066 11.5616 11.5746 14.1906 8.32791 14.1906C5.0812 14.1906 2.44922 11.5616 2.44922 8.31867C2.44922 5.0757 5.0812 2.44676 8.32791 2.44676C11.5746 2.44676 14.2066 5.0757 14.2066 8.31867Z" fill="currentColor"/>
             </svg>

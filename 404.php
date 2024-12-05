f5e0b924bf312303fdf0d7e4abdb8e580f91f0a1
Mykola Bartkiv <bartkivmm93@gmail.com>
2024-12-05 17:44:44 +0200

    Replace Container -> Wrap

--- 404.php
+++ 404.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 <section class="content">
-    <div class="container space tac">
+    <div class="wrap space tac">
         <h1><span>404</span> error</h1>
         <p>Sorry, this page does not exist or has been removed.</p>
     </div>
